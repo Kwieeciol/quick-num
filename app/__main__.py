e6b6a3b6410860272871b31a79c26c649ac47f9c
Kwieeciol <64413721+Kwieeciol@users.noreply.github.com>
2025-03-03 15:43:09 +0100

    set uvloop event loop policy for non-windows host

--- app/__main__.py
+++ app/__main__.py
@@ -1,7 +1,15 @@
+import asyncio
+
 from aiohttp import web
 from .api import create_app
 from .utils import setup_logging
 
+try:
+    import uvloop
+except ImportError:
+    pass
+else:
+    asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 
 if __name__ == '__main__':
     with setup_logging():
